828808af73bb4d3f04b1e34f74ea1b9fbf495748
Aaron Trostle <aaron.trostle@gmail.com>
2013-08-27 00:35:20 -0700

    Add unicorn setup and configure foreman

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TrippingDangerzone::Application.config.secret_token = '77f0dc694ea1ec64efe445a2560265e5440fd6c8ec9116b283318302110cc490d372da80312d33fe26b3d9fee1c5bfcab295461832822fc1f0f0aa733ce55f1c'
+TrippingDangerzone::Application.config.secret_token = ENV["SECRET_TOKEN"]
