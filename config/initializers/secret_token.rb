6a940a045faf900aed5be1f0c1302538a6cd2ba2
Aaron Trostle <aaron.trostle@gmail.com>
2013-08-26 23:52:55 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrippingDangerzone::Application.config.secret_token = '77f0dc694ea1ec64efe445a2560265e5440fd6c8ec9116b283318302110cc490d372da80312d33fe26b3d9fee1c5bfcab295461832822fc1f0f0aa733ce55f1c'
